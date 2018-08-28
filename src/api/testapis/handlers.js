ba63f916cf0d2e44fc366badd4bfbea2a427cb8f
Soham Dodia <sohamdodia@gmail.com>
2018-08-29 00:04:06 +0530

    Added logic for hello route.

--- src/api/testapis/handlers.js
+++ src/api/testapis/handlers.js
@@ -1,4 +1,4 @@
-export let hello = (request, h) => {
-  return 'Hello, world';
-};
+const ucFirst = str => str[0].toUpperCase() + str.slice(1);
+
+export let hello = (request, h) => `Hello, ${request.query.name ? ucFirst(request.query.name) : 'World'}`;
 
